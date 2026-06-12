1a1e4e924a11f8420d1046ebe2c0650e0fa2bb13
fernando.teles <fernando.teles@tcu.gov.br>
2026-06-12 19:55:46 -0300

    feat: add react-microsoft-clarity for enhanced user analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { clarity } from 'react-microsoft-clarity'
 import App from './app/App.tsx'
 import './styles/index.css'
+
+clarity.init('x63nholdpn')
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
